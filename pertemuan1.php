6451467c7b2fc14e678658ac632fd74c8b013d50
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-05-30 19:50:18 +0800

    Menambahkan judul dalam file pertemuan1.php

--- pertemuan1.php
+++ pertemuan1.php
@@ -1,51 +1,52 @@
 <!--
-Procedural Programming
-- Instruksi dilakukan langkah demi langkah
-- Memecah program menjadi bagian-bagian kecil
-- Disebut prosedur, atau subroutine, atau function
-- Linear/Top-to-Bottom
-- Fortran, ALGOL, COBOL, Pascal, C, PHP, JavaScript
+Pendahuluan
+    Procedural Programming
+    - Instruksi dilakukan langkah demi langkah
+    - Memecah program menjadi bagian-bagian kecil
+    - Disebut prosedur, atau subroutine, atau function
+    - Linear/Top-to-Bottom
+    - Fortran, ALGOL, COBOL, Pascal, C, PHP, JavaScript
 
-Kelebihan Procedural Programming
-- To-the-point
-- Simplicity & kemudahan implementasi (untuk compiler dan interpreter)
-- Mudah ditelusuri
-- Membutuhkan lebih sedikit memori (dibandingkan dengan OOP)
+    Kelebihan Procedural Programming
+    - To-the-point
+    - Simplicity & kemudahan implementasi (untuk compiler dan interpreter)
+    - Mudah ditelusuri
+    - Membutuhkan lebih sedikit memori (dibandingkan dengan OOP)
 
-Object Oriented Programming
-- Menyusun semua kode program dan struktur data sebagai objek
-- Objek adalah unit dasar dari program
-- Objek menyimpan data dan perilaku
-- Objek bisa saling berinteraksi
-- Java, Ruby, Python, C++, JavaScript, PHP5
+    Object Oriented Programming
+    - Menyusun semua kode program dan struktur data sebagai objek
+    - Objek adalah unit dasar dari program
+    - Objek menyimpan data dan perilaku
+    - Objek bisa saling berinteraksi
+    - Java, Ruby, Python, C++, JavaScript, PHP5
 
-Kelebihan Object Oriented Programming
-- Representasi dunia nyata
-- Enkapsulasi & Abstraksi Data
-- Reusability
-- Skalabilitas & Ekstensibilitas
-- Kemudahan Pengelolaan
-- Kolaborasi
-- Digunakan oleh framework
+    Kelebihan Object Oriented Programming
+    - Representasi dunia nyata
+    - Enkapsulasi & Abstraksi Data
+    - Reusability
+    - Skalabilitas & Ekstensibilitas
+    - Kemudahan Pengelolaan
+    - Kolaborasi
+    - Digunakan oleh framework
 
-Procedural vs Object Oriented Programming
-- Masing-Masing memiliki kelebihan dan kekurangan
+    Procedural vs Object Oriented Programming
+    - Masing-Masing memiliki kelebihan dan kekurangan
 
-Konsep OOP pada PHP
-- Basic
-    = Class & Object
-    = Property & Method
-    = Constructor
-    = Object Type
-    = Inheritance
-    = Visibility/Access Modifier
-    = Setter & Getter
-    = Static Method
-- Advanced
-    = Abstract & Interface
-    = Interceptor
-    = Object Cloning
-    = Callbacks & Closures
-    = Namespaces & Autoloading
-    = ...
+    Konsep OOP pada PHP
+    - Basic
+        = Class & Object
+        = Property & Method
+        = Constructor
+        = Object Type
+        = Inheritance
+        = Visibility/Access Modifier
+        = Setter & Getter
+        = Static Method
+    - Advanced
+        = Abstract & Interface
+        = Interceptor
+        = Object Cloning
+        = Callbacks & Closures
+        = Namespaces & Autoloading
+        = ...
 -->
