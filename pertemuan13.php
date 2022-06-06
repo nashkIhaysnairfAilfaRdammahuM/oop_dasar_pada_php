8aed492f3dc230ee0e44516302e5da2fa16e793a
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-06-06 19:13:52 +0800

    Menambahkan file pertemuan13.php

---
+++ pertemuan13.php
@@ -0,0 +1,24 @@
+<!--
+Abstract Class
+    - Sebuah kelas yang tidak dapat di-instansiasi
+    - Kelas 'abstrak'
+    - Mendefinisikan interface untuk kelas lain yang menjadi turunannya
+    - Berperan sebagai 'kerangka dasar untuk kelas turunannya'
+    - Memiliki minimal 1 method abstrak; interface
+    - DIgunakan dalam 'pewarisan'/inheritance untuk 'memaksakan' implementasi method abstrak yang sama untuk semua kelas turunannya
+    - Semua kelas turunan, harus mengimplementasikan method abstrak yang ada di kelas abstraknya
+    - Kelas abstrak boleh memiliki property/method reguler
+    - Kelas abstrak boleh memiliki property/static method
+    
+    Method Abstrak
+        - Hanya interface-nya saja
+        - Implementasinya, ada di kelas turunannya
+
+    Kenapa menggunakan kelas abstrak?
+    - Merepresentasikan ide atau konsep dasar
+    - "Composition over Inheritance"
+    - Salah satu cara menerapkan Polimorphism
+    - Sentralisasi logic
+    - Mempermudah pengerjaan tim
+    - 
+-->
