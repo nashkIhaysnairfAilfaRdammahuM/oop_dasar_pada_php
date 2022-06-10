eab68251348f760d7ddd4b7bd83e691acdb5edda
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-06-10 19:35:49 +0800

    Menambahkan folder pertemuan16

---
+++ pertemuan16/App/Produk/Coba.php
@@ -0,0 +1,6 @@
+<?php
+class Coba {
+    public function __construct() {
+        echo "ini adalah kelas Coba";
+    }
+}
---
+++ pertemuan16/index.php
@@ -0,0 +1,21 @@
+<!--
+Autoloading
+    - Jadi gini, kan itu kalau mau buat suatu program pasti butuh banyak require-nya
+    - Autoloading itu bisa load semua saja langsung secara automatis
+    - spl_autoload_register();
+    - spl itu kependekan dari standard php library
+-->
+<?php
+require_once 'App/init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+new Coba();
